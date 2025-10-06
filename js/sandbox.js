2b05e30a6e2c809565a70a5e7032a3c3831db7f8
Anum <127afarghani@frhsd.com>
2025-10-06 12:50:48 -0400

    first commit

---
+++ js/sandbox.js
@@ -0,0 +1,39 @@
+/* console.log(1);
+console.log(2);
+console.log(5+8);
+console.log(10/4);
+
+let age = 25;
+let year = 2019;
+
+console.log(age);
+console.log(age+5);
+
+// log things to console
+console.log(age, year);
+ 
+age=30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
+
+*/
+
+//strings
+
+
+
+//string conactention
+
+
+//getting characters
+
+
+//string length
+
+
+//string methods
