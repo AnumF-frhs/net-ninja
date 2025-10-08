dbbb141a66717f332980d5027430c45ad82c5a1b
Anum <127afarghani@frhsd.com>
2025-10-08 13:11:13 -0400

    yesterday work

--- js/sandbox.js
+++ js/sandbox.js
@@ -24,16 +24,78 @@ console.log(score);
 */
 
 //strings
+console.log('hello,world');
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
 
 
 
 //string conactention
 
+let favFood ='Funnel Cake';
+console.log(favFood);
+
+let day='23';
+let month = '12';
+let year = '2008;' 
+let dateBirth = month +'/' + day +'/' + year
+console.log(dateBirth);
+
+let firstName = 'Anum';
+let lastName = 'Farghani';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+
 
 //getting characters
+console.log(fullName[2])
+
 
 
 //string length
+console.log(fullName.length);
 
 
 //string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+let index = email.indexOf('@');
+console.log(index);
+
+//common string methods
+// let email = 'mario@thenetninja.co.uk';
+//let result = email.lastIndexOf('n');
+//let result = email.slice(0, 5);
+//let result = email.substr(4, 10);
+//let result = email.replace ('m', 'w');
+// let result = email.replaceAll('n', 'w');
+//console.log(result)
+
+let radius= 10;
+const pi = 3.14
+
+//console.log(radius, pi);
+//let result= radius % 3;
+//let result = pi * radius**2;
+
+//order of operation - B I D M A S
+
+//let result = 5 * (10-3)**2;
+
+//console.log(result);
+
+let likes= 10;
+
+//likes = likes +1;
+//likes++;
+//likes--;
+
+//likes +=10;
+//likes -= 5;
+//likes *= 2;
+//likes /= 2;
+
+//console.log(likes);
